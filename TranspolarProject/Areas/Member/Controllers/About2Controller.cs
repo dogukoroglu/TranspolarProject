37505c595a69652048624d037e4100c308ab5452
korog <korog@LAPTOP-GPMKKC5N>
2023-07-10 01:25:41 +0300

    Hakkımızda sayfaları için düzenleme sayfaları eklendi.

---
+++ TranspolarProject/Areas/Member/Controllers/About2Controller.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TranspolarProject.Areas.Member.Controllers
+{
+	[Area("Member")]
+	[Route("Member/About2")]
+	public class About2Controller : Controller
+	{
+		About2Manager about2Manager = new About2Manager(new EfAbout2Dal());
+
+		[Route("")]
+		[Route("Index")]
+		public IActionResult Index()
+		{
+			var values = about2Manager.TGetListAll();
+			return View(values);
+		}
+
+		[Route("UpdateDetails/{id}")]
+		[HttpGet]
+		public IActionResult UpdateDetails(int id)
+		{
+			var value = about2Manager.TGetByID(id);
+			return View(value);
+		}
+
+		[Route("UpdateDetails/{id}")]
+		[HttpPost]
+		public IActionResult UpdateDetails(About2 about2)
+		{
+			about2Manager.TUpdate(about2);
+			return RedirectToAction("Index");
+		}
+
+		[Route("")]
+		[Route("DeleteDetails/{id}")]
+		public IActionResult DeleteDetails(int id)
+		{
+			var value = about2Manager.TGetByID(id);
+			about2Manager.TDelete(value);
+			return RedirectToAction("Index");
+		}
+	}
+}
---
+++ TranspolarProject/Areas/Member/Controllers/AboutController.cs
@@ -0,0 +1,33 @@
+﻿using BusinessLayer.Abstract;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TranspolarProject.Areas.Member.Controllers
+{
+	[Area("Member")]
+	[Route("Member/About")]
+	public class AboutController : Controller
+	{
+		AboutManager aboutManager = new AboutManager(new EfAboutDal());
+
+		[Route("")]
+		[Route("Index")]
+		[HttpGet]
+		public IActionResult Index()
+		{
+			var value = aboutManager.TGetByID(1);
+			return View(value);
+		}
+
+		[Route("")]
+		[Route("Index")]
+		[HttpPost]
+		public IActionResult Index(About about)
+		{
+			aboutManager.TUpdate(about);
+			return RedirectToAction("Index");
+		}
+	}
+}
