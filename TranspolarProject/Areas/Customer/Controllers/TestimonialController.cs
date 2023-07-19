3bb4a2e557196a36259e66f69900a77903a46df6
korog <korog@LAPTOP-GPMKKC5N>
2023-07-19 19:45:26 +0300

    Düzenleme yapıldı

---
+++ TranspolarProject/Areas/Customer/Controllers/TestimonialController.cs
@@ -0,0 +1,57 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace TranspolarProject.Areas.Customer.Controllers
+{
+	[Area("Customer")]
+	[Route("Customer/Testimonial")]
+	public class TestimonialController : Controller
+	{
+		TestimonialManager testimonialManager = new TestimonialManager(new EfTestimonialDal());
+		private readonly UserManager<AppUser> _userManager;
+
+		public TestimonialController(UserManager<AppUser> userManager)
+		{
+			_userManager = userManager;
+		}
+
+		[HttpGet]
+		[Route("AddComment")]
+		public IActionResult AddComment()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[Route("AddComment")]
+		public async Task<IActionResult> AddComment(Testimonial testimonial)
+		{
+			var logginUser = await _userManager.FindByNameAsync(User.Identity.Name);
+			testimonial.Client = logginUser.Name + " " + logginUser.Surname;
+			testimonial.ClientImage = logginUser.ImageUrl;
+			testimonial.ClientJob = "Test";
+			testimonialManager.TAdd(testimonial);
+			return RedirectToAction("Index","Dashboard");
+		}
+
+		//[Route("CommentDetails/{id}")]
+		//[HttpGet]
+		//public IActionResult CommentDetails(int id)
+		//{
+		//	var value = testimonialManager.TGetByID(id);
+		//	return View(value);
+		//}
+
+		//[Route("CommentDetails/{id}")]
+		//[HttpPost]
+		//public IActionResult CommentDetails(Testimonial testimonial)
+		//{
+		//	testimonialManager.TUpdate(testimonial);
+		//	return RedirectToAction("CommentDetails");
+		//}
+	}
+}
